753d5746b9c535c810039aca0ec5437d9eb9aea0
unknown <mehdi.6669@gmail.com>
2023-04-21 15:08:10 +0430

    comment in code

--- cut.py
+++ cut.py
@@ -1,3 +1,4 @@
+# import libraries
 from pyomo.environ import *
 from itertools import combinations
 
@@ -10,28 +11,38 @@ Remains = []
 Drums_count = len(Drums)
 Pieces_count = len(Pieces)
 
+# Create model
 model = ConcreteModel()
 model.piec = Set(initialize=range(Pieces_count))
 model.drum = Set(initialize=range(Drums_count))
 
+# Binary means only accepted value is 0 and 1
 model.x = Var(model.piec*model.drum, initialize=0, within=Binary)
 
+
+# Optimization function
 def ObjRule(model):
    for k in model.drum:
       Remains.append( Drums[k] - sum(Pieces[i]*model.x[i,k] for i in model.piec))
    return (sum((comb[0]-comb[1])**2 for comb in combinations(Remains, 2)))
 
+# Maximize the optimization function
 model.value = Objective(rule=ObjRule, sense = maximize)
 
+# Conditions
+
+# Each piece should cut only one
 model.single_selection = ConstraintList()
 for p in model.piec:
    model.single_selection.add(sum( model.x[p,d] for d in model.drum ) == 1.0)
 
+
+# Sum of cutting pieces from a drum should less than drum length
 model.cutting_limitation = ConstraintList()
 for d in model.drum:
    model.cutting_limitation.add(sum( Pieces[i]*model.x[i,d] for i in model.piec)<=Drums[d])
 
-
+# Download and set solver
 solvername='Couenne'
 
 solverpath_folder='C:\\solver\\Couenne-0.1.1-win32-icl10.0\\bin' 
@@ -46,7 +57,7 @@ results = solver.solve(model,tee=True)
 
 print(results)
 
-
+# Print matrix of cutting
 for r in model.piec:
    for c in model.drum:
       print('(',r,',',c,') = ' + str(model.x[r,c].value))
